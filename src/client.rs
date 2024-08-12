e1e35fbc1400d836a1324b6e53f7620145ee8ba1
mehul20081 <mehul20081@iiitd.ac.in>
2024-08-12 21:50:09 +0530

    client sends 3 sample log messages

--- src/client.rs
+++ src/client.rs
@@ -6,8 +6,16 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut socket = TcpStream::connect("127.0.0.1:8000").await?;
     println!("Connected to the server");
 
-    let msg = b"Hello from client!";
-    socket.write_all(msg).await?;
+    let log_messages = vec![
+        "Log message 1",
+        "Log message 2",
+        "Log message 3",
+    ];
+
+    for message in log_messages {
+        socket.write_all(message.as_bytes()).await?;
+        socket.write_all(b"\n").await?;
+    }
 
     Ok(())
 }
