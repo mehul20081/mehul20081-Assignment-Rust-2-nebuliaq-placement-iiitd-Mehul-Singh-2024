5b946dc3d2ec8d7f71a6263d2a1eb63de3080bcb
mehul20081 <mehul20081@iiitd.ac.in>
2024-08-12 21:59:30 +0530

    client sends log messages at regular interval of 1s

--- src/client.rs
+++ src/client.rs
@@ -1,5 +1,6 @@
 use tokio::net::TcpStream;
 use tokio::io::AsyncWriteExt;
+use tokio::time::{self, Duration};
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -10,9 +11,14 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         "Log message 1",
         "Log message 2",
         "Log message 3",
+        "Log message 4",
+        "Log message 5",
     ];
 
+    let mut interval = time::interval(Duration::from_secs(1));
+
     for message in log_messages {
+        interval.tick().await; 
         socket.write_all(message.as_bytes()).await?;
         socket.write_all(b"\n").await?;
     }
