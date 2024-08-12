b66245fd82d034a8be7d0c34cface40b6828c008
mehul20081 <mehul20081@iiitd.ac.in>
2024-08-12 22:20:34 +0530

    implemented random logs generator in client

--- src/client.rs
+++ src/client.rs
@@ -1,27 +1,26 @@
 use tokio::net::TcpStream;
 use tokio::io::AsyncWriteExt;
 use tokio::time::{self, Duration};
+use rand::Rng;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut socket = TcpStream::connect("127.0.0.1:8000").await?;
     println!("Connected to the server");
 
-    let log_messages = vec![
-        "Log message 1",
-        "Log message 2",
-        "Log message 3",
-        "Log message 4",
-        "Log message 5",
-    ];
-
     let mut interval = time::interval(Duration::from_secs(1));
-
-    for message in log_messages {
-        interval.tick().await; 
-        socket.write_all(message.as_bytes()).await?;
+    let min = 1;
+    let max = 1000;
+    for _ in 0..100{                               //number of log messages to send
+        interval.tick().await;
+        let log_message = generate_log_message(min,max);
+        socket.write_all(log_message.as_bytes()).await?;
         socket.write_all(b"\n").await?;
     }
 
     Ok(())
+}
+fn generate_log_message(min: u32, max: u32) -> String {          //function to crates random log messages
+    let mut random_num = rand::thread_rng();
+    format!("Log message {}", random_num.gen_range(min..=max))
 }
