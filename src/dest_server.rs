3dff832fd138f9208b56acca2a8079fbc44a8125
mehul20081 <88919711+mehul20081@users.noreply.github.com>
2024-08-13 11:42:37 +0530

    Added comments

--- src/dest_server.rs
+++ src/dest_server.rs
@@ -2,16 +2,16 @@ use tokio::net::TcpListener;
 use tokio::io::AsyncReadExt;
 
 #[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let listener = TcpListener::bind("127.0.0.1:9000").await?;
+async fn main() -> Result<(), Box<dyn std::error::Error>> {    //asynchronous functions enables concurrent programming by returning future context
+    let listener = TcpListener::bind("127.0.0.1:9000").await?;   //? is similar to match { Ok()=> , Err()=>) consciecely
     println!("Server listening on 127.0.0.1:9000");
 
     loop {
         let (mut socket, addr) = listener.accept().await?;
-        println!("New client connected: {:?}", addr);
+        println!("New client connected: {:?}", addr);   //:? is debug notation for traits which derives display and debug traits
 
-        tokio::spawn(async move {
-            let mut buffer = [0u8; 1024];
+        tokio::spawn(async move {   //creates an asynchronous task that runs concurrently
+            let mut buffer = [0u8; 1024];   //mutable since we going to update the buffer areay
 
             loop {
                 match socket.read(&mut buffer).await {
@@ -23,7 +23,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
                         println!("Received from {}: {}", addr, String::from_utf8_lossy(&buffer[..n]));
                     }
                     Err(e) => {
-                        eprintln!("Failed to read from socket; err = {:?}", e);
+                        println!("Failed to read from socket; err = {:?}", e);
                         break;
                     }
                 }
